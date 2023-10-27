860c8a410015615e1668316f7b5403e730db4618
FredrikH03 <fredrikholst03@hotmail.com>
2023-10-28 00:36:31 +0200

    bruhwhaddahell idk what im doing

--- program.c
+++ program.c
@@ -8,13 +8,30 @@ int main()
     int length = str_length(chosenWord);
     printf("your chosen word is: %s\n", chosenWord);
     printf("%d\n", length);
+
+    char hiddenWord[10];
+
+    char hiddenWord = hide_word(chosenWord);
+
+    print("%s\n", hiddenWord);
+
     return 0;
 }
     
     
 int str_length(char chosenWord[]) 
+{
+    int count;
+    for(count = 0; chosenWord[count] != '\0'; count++);
+    return count;
+}
+
+int hide_word(chosenWord)
+{
+    char word[10];
+    for(int i = 0; chosenWord[i] != '\0'; i++)
     {
-        int count;
-        for(count = 0; chosenWord[count] != '\0'; count++);
-        return count;
+        word[i] =  "_";
     }
+
+}
