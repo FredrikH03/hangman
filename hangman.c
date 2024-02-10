3aa21802b5021bacba5b618c095262c6e8842cbc
FredrikH03 <fredrikholst03@hotmail.com>
2024-02-10 23:51:55 +0100

    idk trying out if pointers helped, they didnt

--- hangman.c
+++ hangman.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-int main() 
+void main() 
 {
     printf("welcome to hangman!\nplease choose a word to guess!\n");
+
     char chosenWord[50];
     scanf("%s", chosenWord);
+    
     int length = 0;
     length = str_length(chosenWord);
+
     printf("your chosen word is: %s\n", chosenWord);
     printf("%d\n", length);
 
-
     char hiddenWord[50];
-    hide_word(hiddenWord, length);
+    hide_word( & hiddenWord, length);
     
     printf("returned: %s\n", hiddenWord);
 
     return 0;
 }
-    
     
 int str_length(char chosenWord[]) 
 {
@@ -27,12 +28,15 @@ int str_length(char chosenWord[])
     return count;
 }
 
-int hide_word(hiddenWord, length) {
-	char test[50];
+int hide_word(char * hiddenWord[], int length) {
+	char temp[50];
+
 	for (int i = 0; i < length; i++) {
-		test[i] = '_';
+		temp[i] = '_';
 	}
-	test[length] = '\0';
-	printf("%s\n", test);
-	hiddenWord = test;
+	temp[length] = '\0';
+
+	printf("value in function: %s\n", temp);
+	hiddenWord = temp;
+
 }
